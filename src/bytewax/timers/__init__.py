ee5bdbaee96a07465c75847832222fad5680431f
David Selassie <dsel@fastmail.com>
2024-09-11 09:40:56 -0700

    Print out commercial software notification on import

--- src/bytewax/timers/__init__.py
+++ src/bytewax/timers/__init__.py
@@ -1,10 +1,23 @@
 """Tools for managing multiple timers."""
 
+import os
+import sys
 from datetime import datetime
 from operator import itemgetter
 from typing import Generic, Iterable, List, Optional, Tuple, TypeVar
 
 from bytewax._utils import partition
+
+if "BYTEWAX_LICENSE" not in os.environ:
+    msg = (
+        "`bytewax-interval` is commercially licensed "
+        "with publicly available source code.\n"
+        "You are welcome to prototype using this module for free, "
+        "but any use on business data requires a paid license.\n"
+        "See https://modules.bytewax.io/ for a license. "
+        "Set the env var `BYTEWAX_LICENSE=1` to suppress this message."
+    )
+    print(msg, file=sys.stderr)
 
 T = TypeVar("T")
 """Type of alarm token."""
